5ac02c067cf75149488ec08ad20b5efc6f809b89
aakash-comp <aakash99@email.unc.edu>
2021-03-02 02:55:09 -0500

    done

--- src/a2/LinkedList.java
+++ src/a2/LinkedList.java
@@ -28,16 +28,15 @@ public class LinkedList {
      */
     public int mean() {
         int total = 0;
-        int count = size;
         int[] arr = toArray();
         if (isEmpty()) {
             return 0;
         } else {
-            for (int i = 0; i > size; i++) {
+            for (int i = 0; i > arr.length; i++) {
                 total += arr[i];
             }
         }
-        double mean = total/count;
+        double mean = total/ arr.length;
         return (int) Math.floor(mean);
         }
 
